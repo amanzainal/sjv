13dd5b6f24b38d2c91060563090f9ce47d7cd40d
aman zainal <aman@u.nus.edu>
2024-03-05 02:33:49 +0800

    Fix typo

--- sjv.bat
+++ sjv.bat
@@ -4,7 +4,7 @@ if "%~1" == "11" (
 ) else if "%~1" == "19" (
    set "JAVA_HOME=C:\Program Files\Java\jdk-19"
 ) else (
-   set "JAVA_HOME=C:\Program Files\Java\jdk-19"
+   set "JAVA_HOME=C:\Program Files\Java\jdk-17"
 )
 setx JAVA_HOME "%JAVA_HOME%" /m
 set "Path=%JAVA_HOME%\bin;%Path%"
