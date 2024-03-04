b4fd7f0e3ef537d2d94782d8831be1e7c5c55b76
aman zainal <aman@u.nus.edu>
2024-03-05 02:32:34 +0800

    Fix sysenv not updating system-wide

--- sjv.bat
+++ sjv.bat
@@ -1,8 +1,11 @@
 @echo off
 if "%~1" == "11" (
    set "JAVA_HOME=C:\Program Files\Java\jdk-11"
+) else if "%~1" == "19" (
+   set "JAVA_HOME=C:\Program Files\Java\jdk-19"
 ) else (
-   set "JAVA_HOME=C:\Program Files\Java\jdk-17"
+   set "JAVA_HOME=C:\Program Files\Java\jdk-19"
 )
+setx JAVA_HOME "%JAVA_HOME%" /m
 set "Path=%JAVA_HOME%\bin;%Path%"
 java -version
